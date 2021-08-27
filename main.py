910d4c45ef0940a62030055d9abd216e19f3f6e5
hyunseo <82203978+hyena0608@users.noreply.github.com>
2021-08-27 11:58:23 +0000

    210827

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-result = str(5 * 5 * 4)
+n, m = map(int, input().split())
 
-print(result)
-print(result.count(str(0)))
+graph = [[0] * m for _ in range(n)]
+
+# print(graph)
+for i in range(n):
+  graph[i] = input() 
+
+for i in range(n):
+  for j in range(n):
+    
+# # # 상 하 좌 우
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# for
