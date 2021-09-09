c0d9cdc205ba060c6d5f20f7a70660c46ed6f18e
hyunseo <82203978+hyena0608@users.noreply.github.com>
2021-09-09 22:59:47 +0000

    210910

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-x = int(input())
+arr = list(map(int, input()))
+half = int(len(arr)/2)
+a = sum(arr[0:half])
+b = sum(arr[half:])
 
-stars = [[' ' for _ in range(x)] for _ in range(x)]
-
-def all_star(n, x, y):
-    if n == 1:
-        stars[y][x] = '*'
-        
-    else:
-        m = n // 3
-        for dy in range(3):
-            for dx in range(3):
-                if dy != 1 or dx != 1:
-                    all_star(m, x+dx*m, y+dy*m)
-                    
-all_star(x, 0, 0)
-for i in stars:
-    print(''.join(i))
+if a == b:
+  print("LUCKY")
+else:
+  print("READY")
