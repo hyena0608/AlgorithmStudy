60d774091e67db5e9e4fa47fc2d58a4d09adca76
hyunseo <82203978+hyena0608@users.noreply.github.com>
2021-09-27 14:17:18 +0000

    210927

---
+++ main.py
@@ -0,0 +1,17 @@
+n,m = list(map(int,input().split()))
+ 
+s = []
+ 
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            print(i)
+            dfs()
+            s.pop()
+ 
+dfs()
