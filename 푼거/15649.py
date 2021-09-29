b0f3110de5d37e03b6480ffd768caa2d5743f8c5
hyunseo <82203978+hyena0608@users.noreply.github.com>
2021-09-29 13:40:22 +0000

    210929

--- 푼거/15649.py
+++ 푼거/15649.py
@@ -3,15 +3,14 @@ n, m = map(int, input().split())
 arr = []
 
 def dfs():
-    if len(arr) == m :
+    if len(arr) == m:
         print(' '.join(map(str, arr)))
         return
     
     for i in range(1, n + 1):
         if i not in arr:
-            if len(arr) == 0 or max(arr) < i:
-                arr.append(i)
-                dfs()
-                arr.pop()
+            arr.append(i)
+            dfs()
+            arr.pop()
 
 dfs()
