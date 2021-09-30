7736d296b0c5d99b896e6147c633b57d5c3ebec3
hyunseo <82203978+hyena0608@users.noreply.github.com>
2021-09-30 14:31:18 +0000

    210930

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-n, m = map(int, input().split())
+# 홀 짝 1 3 7 15
+# 2^n - 1
 
-arr = []
+a = int(input())
 
-def dfs():
-    if len(arr) == m :
-        print(' '.join(map(str, arr)))
-        return
-    
-    for i in range(1, n + 1):
-        if i not in arr:
-            if len(arr) == 0 or max(arr) < i:
-                arr.append(i)
-                dfs()
-                arr.pop()
+graph = []
+stars = []
 
-dfs()
+def star(n):
+    if n == 1:
+        graph.append('*')
+        stars.append('*')
+    else:
+        if n % 2 == 0:
+            stars.append('*' + graph[len(graph) - 1] + '*' + graph[len(graph) - 1] + '*')
+            for space in range((2**n - 3), 3, -2):  
+                stars.append(' ' * space + '*' + ' ' * space + '*' + ' ' * space + '*')
+            stars.append('*' + graph[len(graph) - 1] + '*')
+            for space in range((2**n - 4), 2, -2):
+                stars.append(' ' * space + '*' + ' ' * space + '*')     
+        else:
+            stars.append(' ' * (len(graph[0]) + 1) + '*')
+            for space in range(1, (2**n - 1) // 2 + 1):
+                stars.append(' ' * (len(graph[0]) + 1 - space) + '*' + ' ' * (2**space - 1) + '*')
+            stars.append('*' + graph[0] + '*')
+            for space in range(1, 2**n - 1):
+                stars.append(' ' * space + '*' + ' ' * (2**space)  + graph[space] + ' ' * (2**space) + '*')
+            stars.append('*' + graph[0] + '*' + graph[0] + '*')
+
+for n in range(1, a + 1):
+    print(n)
+    star(n)
+    graph = stars
+    # print(graph)
+    # print(stars)
+
+for star in graph:
+    print(star)
+
+
+# print(len('            '))
