3d758311911758d282d24e6037885690126d1302
hyena0608 <goatsfish@gmail.com>
2022-03-16 00:20:12 +0900

    20220315

--- 인프런_자바_알고리즘_문제_기초/팰린드롬.java
+++ 인프런_자바_알고리즘_문제_기초/팰린드롬.java
@@ -2,18 +2,18 @@ package 인프런_자바_알고리즘_문제_기초;
 
 import java.util.Scanner;
 
-public class 회문문자열 {
+public class 팰린드롬 {
     public String solution(String str) {
-        str = str.toUpperCase();
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
         String tmp = new StringBuilder(str).reverse().toString();
         if (str.equals(tmp)) return "YES";
         else return "NO";
     }
 
     public static void main(String[] args) {
-        회문문자열 T = new 회문문자열();
+        팰린드롬 T = new 팰린드롬();
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.nextLine();
         System.out.println(T.solution(str));
     }
 }
