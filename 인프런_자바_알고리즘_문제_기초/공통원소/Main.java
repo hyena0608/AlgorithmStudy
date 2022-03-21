444728b20097cd83896faedfd6e3c17954617612
hyena0608 <goatsfish@gmail.com>
2022-03-21 23:20:07 +0900

    20220321

---
+++ 인프런_자바_알고리즘_문제_기초/공통원소/Main.java
@@ -0,0 +1,20 @@
+package 인프런_자바_알고리즘_문제_기초.공통원소;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class Main {
+    public ArrayList<Integer> solution(int n, int m, int[] arrA, int[] arrB) {
+
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
