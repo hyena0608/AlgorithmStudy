68d34dbc0828642c9ed3d5e00db2557a5661c385
hyena0608 <goatsfish@gmail.com>
2022-05-15 17:21:58 +0900

    20220515

--- 백준/자바/요세푸스문제/Main.java
+++ 백준/자바/요세푸스문제/Main.java
@@ -1,38 +1,28 @@
 package 백준.자바.요세푸스문제;
 
 import java.util.*;
-
-// 14
-
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        StringBuffer stringBuffer = new StringBuffer();
+        Queue<Integer> Q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) Q.add(i);
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++) list.add(i);
-
-        int index = m - 1;
-        int[] answer = new int[n];
-        answer[0] = list.remove(index);
-        for (int i = 1; i < n; i++) {
-            if (list.size() <= m) {
-                answer[i] = list.remove(0);
-            } else {
-                if (index + m > list.size() - 1) {
-                    index = m + index - (list.size() - 1) - 1;
-                    // 1245
-                } else {
-                    index += m;
-                }
-                answer[i] = list.remove(index);
-            }
+        while (n --> 0) {
+            recursionMTimes(Q, m, stringBuffer);
         }
 
-        for (int i : answer) {
-            System.out.println(i);
+        stringBuffer.replace(stringBuffer.length() - 2, stringBuffer.length() - 1, ">");
+        System.out.print("<" + stringBuffer);
+    }
+
+    public static void recursionMTimes(Queue<Integer> Q, int m, StringBuffer stringBuffer) {
+        for (int i = 0; i < m - 1; i++) {
+            Q.add(Q.poll());
         }
+        stringBuffer.append(Q.poll()).append(", ");
     }
 }
