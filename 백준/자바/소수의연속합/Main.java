385efaa9b0f3134421654b50ff03809af4a3f32e
hyena0608 <goatsfish@gmail.com>
2022-05-24 06:57:56 +0900

    20220524

--- 백준/자바/소수의연속합/Main.java
+++ 백준/자바/소수의연속합/Main.java
@@ -3,44 +3,32 @@ package 백준.자바.소수의연속합;
 import java.util.*;
 
 public class Main {
-
     static List<Integer> sosuList;
-    static Map<Integer, Integer> sumMap;
-    static List<Point> pointList;
+    static int answer;
+    static int n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         sosuList = new ArrayList<>();
-        sumMap = new HashMap<>();
-        pointList = new ArrayList<>();
+        answer = 0;
 
-        makeSosuList(sosuList, n);
-        collectPoints(0);
-        addAllSumToMap();
+        Main T = new Main();
 
-        if (sumMap.containsKey(n)) {
-            System.out.println(sumMap.get(n));
+        T.makeSosuList(sosuList);
+
+        for (int i = 0; i < sosuList.size(); i++) {
+            T.c(i, 0);
+        }
+
+        if (T.isSosu(n)) {
+            System.out.println(answer);
         } else {
             System.out.println(0);
         }
     }
 
-    private static void addAllSumToMap() {
-        for (Point point : pointList) {
-            int sum = 0;
-            for (int i = point.start; i <= point.end; i++) {
-                sum += sosuList.get(i);
-            }
-            sumMap.put(sum, sumMap.getOrDefault(sum, 0) + 1);
-        }
-        for (Integer sosu : sosuList) {
-            sumMap.put(sosu, sumMap.getOrDefault(sosu, 0) + 1);
-
-        }
-    }
-
-    public static void makeSosuList(List<Integer> sosuList, int n) {
+    private void makeSosuList(List<Integer> sosuList) {
         for (int num = 2; num <= n; num++) {
             if (isSosu(num)) {
                 sosuList.add(num);
@@ -48,9 +36,9 @@ public class Main {
         }
     }
 
-    private static boolean isSosu(int n) {
+    private boolean isSosu(int n) {
         int sqirtN = (int) Math.sqrt(n);
-        for (int num = 2; num <= sqirtN; num++) {
+        for (int num = 2; num <= sqirtN + 1; num++) {
             if (n % num == 0) {
                 return false;
             }
@@ -58,22 +46,16 @@ public class Main {
         return true;
     }
 
-    public static void collectPoints(int L) {
+    private void c(int L, int sum) {
         for (int i = L; i < sosuList.size(); i++) {
-            for (int j = L + 1; j < sosuList.size(); j++) {
-                if (sosuList.get(i) < sosuList.get(j))
-                    pointList.add(new Point(i, j));
+            if (sum + sosuList.get(i) > n) {
+                break;
+            } else if (sum + sosuList.get(i) == n) {
+                answer++;
+                break;
+            } else {
+                sum += sosuList.get(i);
             }
         }
     }
-
-    static class Point {
-        int start, end;
-
-        public Point(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
 }
-
