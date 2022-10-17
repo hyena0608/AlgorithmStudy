f2c1f48e1973d5aa89a3db0c7d97106d88d09c44
hyena0608 <goatsfish@gmail.com>
2022-10-18 06:54:04 +0900

    [완전탐색] : 2문제

--- 코트/구현력/완전탐색/기준새로설정/구간잘나누기/Main.java
+++ 코트/구현력/완전탐색/기준새로설정/구간잘나누기/Main.java
@@ -2,11 +2,11 @@ package 코트.구현력.완전탐색.기준새로설정.구간잘나누기;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int MAX_ARR_SIZE = 200;
+    private static final int MAX_ARR_SIZE = 100;
 
     private static int N, M;
     private static int[] arr;
@@ -23,100 +23,34 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i * 2] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int maxButMin = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE;
+        for (int findMax = 1; findMax <= 10000; findMax++) {
 
-        List<Point> stores = new ArrayList<>();
-        Queue<Point> Q = new LinkedList<>();
-        Q.add(new Point(new ArrayList<>()));
-        while (!Q.isEmpty()) {
-            Point curr = Q.poll();
-
-            if (curr.checkStickSizeFull()) {
-                maxButMin = Math.min(maxButMin, findMax(curr.getStickIndexes()));
-                continue;
-            }
-
-            for (int idx = 1; idx < N; idx++) {
-                int stickIdx = idx * 2 - 1;
-
-                if (!curr.checkStickExists(stickIdx)) {
-                    curr.addStick(stickIdx);
-
-                    boolean checkAlreadyExists = stores.stream()
-                            .anyMatch(store -> store.checkSame(curr.getStickIndexes()));
-
-                    if (!checkAlreadyExists) {
-                        Q.add(curr.clone());
-                        stores.add(curr.clone());
-                    }
-
-                    curr.removeStick(stickIdx);
+            boolean isPossible = true;
+            int subCount = 1;
+            int currValue = 0;
+            for (int curr = 0; curr < N; curr++) {
+                if (arr[curr] > findMax) {
+                    isPossible = false;
+                    break;
                 }
+
+                if (currValue + arr[curr] > findMax) {
+                    currValue = 0;
+                    subCount++;
+                }
+
+                currValue += arr[curr];
+            }
+
+            if (isPossible && subCount <= M) {
+                answer = Math.min(answer, findMax);
             }
         }
 
-        System.out.println(maxButMin);
-
-    }
-
-    public static int findMax(List<Integer> stickIndexes) {
-        stickIndexes.add(0);
-        stickIndexes.add(N * 2);
-        Collections.sort(stickIndexes);
-
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < M; i++) {
-            int left = stickIndexes.get(i);
-            int right = stickIndexes.get(i + 1);
-            int sum = 0;
-            for (int j = left; j <= right; j++) {
-                sum += arr[j];
-            }
-            max = Math.max(max, sum);
-        }
-
-        return max;
-    }
-
-    static class Point {
-        private final List<Integer> stickIndexes;
-
-        public Point(List<Integer> sticks) {
-            stickIndexes = new ArrayList<>();
-            stickIndexes.addAll(sticks);
-        }
-
-        public void addStick(Integer stick) {
-            if (stickIndexes.contains(stick)) return;
-            stickIndexes.add(stick);
-        }
-
-        public void removeStick(Integer stick) {
-            if (!stickIndexes.contains(stick)) return;
-            stickIndexes.remove(stick);
-        }
-
-        public boolean checkStickSizeFull() {
-            return stickIndexes.size() == M - 1;
-        }
-
-        public boolean checkStickExists(int stick) {
-            return stickIndexes.contains(stick);
-        }
-
-        public boolean checkSame(List<Integer> sticks) {
-            return new HashSet<>(stickIndexes).containsAll(sticks);
-        }
-
-        public List<Integer> getStickIndexes() {
-            return stickIndexes;
-        }
-
-        public Point clone() {
-            return new Point(this.getStickIndexes());
-        }
+        System.out.print(answer);
     }
 }
--- 코트/구현력/완전탐색/기준새로설정/언덕깎기/Main.java
+++ 코트/구현력/완전탐색/기준새로설정/언덕깎기/Main.java
@@ -4,35 +4,39 @@ import java.io.*;
 
 public class Main {
 
-    public static final int MAX_H = 100;
-    public static final int MAX_N = 1000;
-    public static final int K = 17;
-
-    public static int N;
-    public static int[] arr;
+    private static final int MAX_GAP = 17;
+    private static final int MAX_N = 1000;
+    private static final int MAX_H = 100;
+    private static int N;
+    private static int[] arr;
 
     public static void main(String[] args) throws IOException {
         arr = new int[MAX_N];
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int minCost = Integer.MAX_VALUE;
-        for (int i = 0; i <= MAX_H; i++) {
+        // baseHeight ~ baseHeight + MAX_GAP 구간이라고 가정하고 진행한다.
+        int answer = Integer.MAX_VALUE;
+        for (int baseHeight = 0; baseHeight <= MAX_H; baseHeight++) {
             int cost = 0;
-            for (int j = 0; j < N; j++) {
-                if (arr[j] < i)
-                    cost += (arr[j] - i) * (arr[j] - i);
-                if (arr[j] > i + K)
-                    cost += (arr[j] - i - K) * (arr[j] - i - K);
+            for (int idx = 0; idx < N; idx++) {
+                if (arr[idx] < baseHeight) {
+                    cost += Math.pow((arr[idx] - baseHeight), 2);
+                }
+                if (arr[idx] > baseHeight + MAX_GAP) {
+                    cost += Math.pow((arr[idx] - MAX_GAP - baseHeight), 2);
+                }
             }
-            minCost = Math.min(minCost, cost);
+
+            answer = Math.min(answer, cost);
         }
 
-        System.out.print(minCost);
+        System.out.print(answer);
     }
+
 }
